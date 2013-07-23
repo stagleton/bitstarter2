b80ebf8699bcabe503a3de2c214f8ee7d7023244
Ubuntu <ubuntu@ip-10-164-62-178.ec2.internal>
2013-07-23 15:11:45 +0000

    Updated web.js and index.html to show crowdstarter website

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+app.get('/', function(request, response){
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);  
+  });
+  
+var port = process.env.PORT || 8080;
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, function(){
+    console.log("Listening on " + port);
+  });
